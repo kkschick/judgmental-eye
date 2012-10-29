12456e449d186c7ada02252e90343ea0a5692114
chriszf <chriszf@gmail.com>
2012-10-29 08:47:11 -0700

    Adding requirements, skeleton model

---
+++ model.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+
+
+
+def main():
+    """In case we need this for something"""
+    pass
+
+if __name__ == "__main__":
+    main()
