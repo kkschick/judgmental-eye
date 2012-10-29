0e325467e641de2984a906b7bf59bfecc1dcac64
chriszf <chriszf@gmail.com>
2012-10-29 12:32:20 -0700

    Adding seed.py skeleton

---
+++ seed.py
@@ -0,0 +1,22 @@
+import model
+import csv
+
+def load_users(session):
+    # use u.user
+    pass
+
+def load_movies(session):
+    # use u.item
+    pass
+
+def load_ratings(session):
+    # use u.data
+    pass
+
+def main(session):
+    # You'll call each of the load_* functions with the session as an argument
+    pass
+
+if __name__ == "__main__":
+    s= model.connect()
+    main(s)
